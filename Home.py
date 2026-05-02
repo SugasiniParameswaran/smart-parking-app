aa9f464b7817ab14356874bf9ad785ee384aa9fa
SugasiniParameswaran <144006326+SugasiniParameswaran@users.noreply.github.com>
2026-05-02 08:31:55 +0530

    Home.py

--- Home.py
+++ Home.py
@@ -1,9 +1,5 @@
 import streamlit as st
 
-# Initialize page
-if "page" not in st.session_state:
-    st.session_state.page = "home"
-
 st.title("Smart Parking System")
 st.subheader("Login Screen")
 
@@ -11,13 +7,6 @@ role = st.radio("Login as", ["Driver", "Admin"])
 
 if st.button("Login"):
     if role == "Driver":
-        st.session_state.page = "driver_login"
+        st.success("👉 Go to 'Driver_Login' from the sidebar")
     else:
-        st.session_state.page = "admin_login"
-
-# Navigation
-if st.session_state.page == "driver_login":
-    st.switch_page("pages/Driver_Login.py")
-
-elif st.session_state.page == "admin_login":
-    st.switch_page("pages/Admin_Login.py")
+        st.success("👉 Go to 'Admin_Login' from the sidebar")
