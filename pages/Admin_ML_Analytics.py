fec7f4006885633cdbd558693f7ce21f99a04ce7
SugasiniParameswaran <144006326+SugasiniParameswaran@users.noreply.github.com>
2026-05-02 14:00:23 +0530

    Admin_ML_Analytics.py

--- pages/Admin_ML_Analytics.py
+++ pages/Admin_ML_Analytics.py
@@ -125,10 +125,16 @@ ax3.set_title(f"Predicted Customers by Time for {selected_month_name} {selected_
 ax3.grid(True)
 
 st.pyplot(fig3)
-st.markdown("---")
-
-if st.button("🔙 Back to Home"):
-    st.markdown(
-        '<a href="/" target="_self">Go to Home</a>',
-        unsafe_allow_html=True
-    )
+st.markdown("""
+    <a href="/" target="_self">
+        <button style="
+            padding:10px 20px;
+            background-color:#4CAF50;
+            color:white;
+            border:none;
+            border-radius:8px;
+            cursor:pointer;">
+            🔙 Back to Home
+        </button>
+    </a>
+""", unsafe_allow_html=True)
